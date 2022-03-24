929426b54c1978f9b8f8ea5aa1043def1f7732c5
Jeff Overholt <jeffrey.a.overholt@usps.gov>
2022-03-23 20:54:48 -0400

    adding queries file

---
+++ queries.sql
@@ -0,0 +1,32 @@
+-- Create tables for raw data to be loaded into
+CREATE TABLE County (
+id INT PRIMARY KEY,
+fips INT,
+county TEXT,
+state_abbreviation TEXT,
+2014_pop INT,
+pct_pop_delt10to14 INT,
+2010_pop INT,
+pct_hs_grad INT,
+pct_post_bach_grad INT,
+per_capita_income INT,
+median_household_income INT,
+sq_miles INT,
+pop_per_sq_mile INT,
+state_name TEXT
+);
+
+CREATE TABLE Police (
+id INT PRIMARY KEY,
+Total_Law_Enforcement_Employees INT,
+Total_Officers INT,
+Total_Civilians INT,
+state_name TEXT
+);
+
+
+-- -- Joins tables
+-- SELECT customer_name.id, customer_name.first_name, customer_name.last_name, customer_location.address, customer_location.us_state
+-- FROM customer_name
+-- JOIN customer_location
+-- ON customer_name.id = customer_location.id;
